6a012d6a41cc636377dd0df3292f6d62cc71fc52
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-07-02 22:39:18 -0700

    Can make ftp style addresses

--- FTP.hs
+++ FTP.hs
@@ -4,6 +4,8 @@
 
 module FTP where
 
+import Network.Socket
+
 -- FTP commands are defined as (Command, Argument)
 type Command = (String, String)
 
@@ -18,3 +20,17 @@ makeCommand s
 -- Returns only the action part of an FTP command
 getCommand :: Command -> String
 getCommand (c, _) = c
+
+-- Takes a socket address, returns it in proper FTP string format.
+-- Host strings are in the format "h1,h2,h3,h4,p1,p2" as described
+-- here: http://cr.yp.to/ftp/retr.html
+getFTPAddr :: SockAddr -> String
+getFTPAddr address = let addr = (show address) 
+	in parse addr
+	where 
+		parse (x:xs)
+			| (length xs == 0) = ""
+			| ([x] == ":") = ""
+			| ([x] == ".") = [','] ++ (parse xs)
+			| otherwise = [x] ++ (parse xs)
+		parse [] = ""
