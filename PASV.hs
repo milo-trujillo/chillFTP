95927e9704523c54ff996ce68a838efa456a2798
Milo Trujillo <milo.trujillo@sonomaacademy.org>
2014-07-03 20:37:11 -0700

    Extended PASV support and debugging

--- PASV.hs
+++ PASV.hs
@@ -1,6 +1,8 @@
 {-
-	This module handles passive data connections for FTP.
+	This module is responsible for managing the passive data port.
+
 	We only export the main function, openPASV, all other code is private.
+	Interaction between the control and data ports is done via a channel.
 -}
 
 module PASV (openPASV) where
@@ -9,26 +11,65 @@ import System.IO
 import Network.Socket
 import Control.Concurrent
 
+import FTP
+
 -- We open one port per user data connection, so the connection limit is 1
 max_connections :: Int
 max_connections = 1
 
 -- Opens a new passive ftp connection, returns the port number
-openPASV :: IO Int
-openPASV = do
+openPASV :: Chan Command -> IO Int
+openPASV commands = do
 	sock <- socket AF_INET Stream 0		-- Make new socket
 	setSocketOption sock ReuseAddr 1	-- Make reusable listening sock
 	bindSocket sock (SockAddrInet aNY_PORT iNADDR_ANY)
 	listen sock max_connections
-	_ <- forkIO (handlePASV sock)
-	addr <- getSocketName sock
-	putStrLn (show addr)
+	allowed_addr <- getAddr
+	putStrLn ("Allowing connections from " ++ allowed_addr)
+	_ <- forkIO (acceptPASV sock allowed_addr commands)
 	portno <- socketPort sock
 	return (read (show (portno)) :: Int)
+	where
+		-- We need to get the address connections are allowed from
+		-- It's supplied over the channel like ("PASV", "10,0,0,2")
+		getAddr = do
+			command <- readChan commands
+			let (action, arg) = command
+			if (action == "PASV") then
+				return arg
+			else
+				getAddr
+
+-- Accepts a connection, if it's from the allowed address
+-- Runs recursively until success
+acceptPASV :: Socket -> String -> Chan Command -> IO ()
+acceptPASV data_listen allowed commands = do
+	(sock, address) <- Network.Socket.accept data_listen
+	s <- socketToHandle sock ReadWriteMode
+	hSetNewlineMode s (NewlineMode { inputNL = CRLF, outputNL = LF })
+	hSetBuffering s NoBuffering
+	let addr = getFTPAddr address
+	putStrLn ("Opened PASV from " ++ addr)	-- DEBUG
+	if (addr == allowed) then do
+		handlePASV s commands
+	else do
+		hPutStrLn s "Access denied."
+		hClose s
+		acceptPASV data_listen allowed commands
 
 -- Handles a passive ftp connection once a user connects
-handlePASV :: Socket -> IO ()
-handlePASV pasv = do
-	(sock, _) <- Network.Socket.accept pasv
-	s <- socketToHandle sock ReadWriteMode
-	hPutStrLn s "Welcome to passive!"
+handlePASV :: Handle -> Chan Command -> IO ()
+handlePASV s commands = do
+	(command, _) <- readChan commands
+	putStrLn ("Read command: " ++ command)	-- DEBUG
+	case command of
+		"LIST"	->	hPutStrLn s "150 Opening ASCII connection for file list" >>
+					hPutStrLn s "... No files found ..." >>
+					hPutStrLn s "226 Transfer complete fools."
+		"QUIT"	->	hClose s
+		_		->	hPutStrLn s "502 Command not supported in PASV"
+	streamOpen <- hIsOpen s
+	if streamOpen then
+		handlePASV s commands
+	else
+		return ()
